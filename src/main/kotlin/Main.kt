a321563ea0371a9e2a2bab49580ba6a92d003c5c
Ильяс Шамсутдинов <ilyaskrut5000@gmail.com>
2023-01-18 17:22:33 +0500

    Refinement commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 fun main() {
-    val amount = 12513652
+    val amount = 15687400
     val comission = 0.75
     val minimumComission = 3500
     val calculation = (amount * comission) / 100
     val result = if (calculation.toInt() < minimumComission) minimumComission else calculation.toInt()
-    println ("Ваша комиссия: " + result + " коп.")
+    println ("Ваша комиссия: " + result/100 + " руб.")
 
 }
